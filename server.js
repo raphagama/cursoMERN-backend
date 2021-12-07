3c60e05f33c36852b52467c511912a172d8bf450
Raphael Gama <raphaelgama19@gmail.com>
2021-12-07 10:47:02 -0300

    Configura porta do Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const routes = require('./src/routes');
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = 5000;
 
 mongoose.connect('mongodb://localhost:27017/database', {
     userUnifiedTopology: true,
@@ -26,6 +26,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () =>{
+app.listen(process.env.PORT || port, () =>{
     console.log(`Server runing on port ${port}`)
 })
